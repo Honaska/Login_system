f5c43b846fa16040b844b643d2f92eff5a04ba80
Honaska <tantoyjamesryan@gmail.com>
2023-01-09 12:15:52 +0800

    Login Example

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:examples/login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginDemo(),
+    );
+  }
+}
+
+
